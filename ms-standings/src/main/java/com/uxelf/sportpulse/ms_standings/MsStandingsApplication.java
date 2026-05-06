592324831a8f8153333b29f23f56b38b0ecbef84
Uxelf <uxself@gmail.com>
2026-05-06 11:17:03 +0200

    Clean start

---
+++ ms-standings/src/main/java/com/uxelf/sportpulse/ms_standings/MsStandingsApplication.java
@@ -0,0 +1,13 @@
+package com.uxelf.sportpulse.ms_standings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsStandingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsStandingsApplication.class, args);
+	}
+
+}
